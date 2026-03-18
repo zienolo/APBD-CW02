790ece52c1370c41a65bf1d6e070dc85a8176c66
ziens <zienek12319@wp.pl>
2026-03-18 21:56:38 +0100

    Added StatisticHelper Class and function addValue

---
+++ Tutorial2/Tutorial2/validation.cs
@@ -0,0 +1,21 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace Tutorial2;
+
+public class StatisticsHelper
+{
+    private List<int> Database;
+
+
+    public StatisticsHelper()
+    {
+        Database = new List<int>();
+    }
+
+    public void addValue(int value)
+    {
+        Database.Add(value);
+    }
+    
+
+}
